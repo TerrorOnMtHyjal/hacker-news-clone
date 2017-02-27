30e1e799568aac03718e787827f5f0c53126abce
marcy <marcy.montross@gmail.com>
2017-02-27 16:24:47 -0600

    PUT test

--- test/test-server.js
+++ test/test-server.js
@@ -70,4 +70,49 @@ describe('Hacker News API', function() {
       });
     });
   });
+  describe('GET all stories', function() {
+    it('should return all stories', function() {
+      let capturedRes;
+      return chai.request(app)
+      .get('/stories')
+      .then(function(res){
+        capturedRes = res;
+        capturedRes.should.have.status(200);
+        capturedRes.body.Stories.should.have.length.of.at.least(1);
+        return Story.count();
+      })
+      .then(function(count){
+        console.log('count: ', count);
+        capturedRes.body.Stories.should.have.length.of(count);
+      });
+    });
+  });
+
+  describe('PUT update', function() {
+    it('should update story', function(){
+      const updatedStoryDetails = {
+        title: "This is a Title",
+        url: "www.myhackernewssomethingorother.com"
+      };
+      return Story
+        .findOne().exec()
+        .then(function(storyToUpdate){
+          updatedStoryDetails.id = storyToUpdate.id;
+          return chai.request(app)
+            .put( `/stories/${storyToUpdate.id}`)
+            .send(updatedStoryDetails);
+        })
+        .then(function(res) {
+          res.should.have.status(200);
+          res.should.be.json;
+          return Story.findById(updatedStoryDetails.id).exec();
+        })
+        .then(function(story){
+          story.title.should.equal(updatedStoryDetails.title);
+          story.url.should.equal(updatedStoryDetails.url);
+        });
+    });
+  });
+
+
 });
