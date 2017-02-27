88cb02eacd44763fa7a6003648808e16d3a03a21
dustwise <jared.mohney@gmail.com>
2017-02-27 11:44:29 -0700

    afternoon workshop changes

--- server.js
+++ server.js
@@ -18,8 +18,9 @@ app.post('/stories', (req, res) => {
   for (let i=0; i< requiredFields.length; i++){
     const field = requiredFields[i];
     if(!(field in req.body)){
-      console.error(`Missing ${field} in request`);
-      return res.status(400);
+      const message = `Missing ${field} in request`;
+      console.error(message);
+      return res.status(400).send(message);
     }
   }
 
