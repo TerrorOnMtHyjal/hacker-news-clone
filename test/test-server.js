7a79f9e1044a139e043683bac4ff0f8b1e78aef3
marcy <marcy.montross@gmail.com>
2017-02-27 15:37:24 -0600

    create test

--- test/test-server.js
+++ test/test-server.js
@@ -2,16 +2,28 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
 const faker = require('faker');
-
+const {Story} = require('../models');
 const should = chai.should();
 
 const {app, runServer, closeServer} = require('../server');
 
+mongoose.Promise = global.Promise;
 
 chai.use(chaiHttp);
 
+function generateStory(){
+  return {
+    title: faker.lorem.sentence(),
+    url: faker.url
+  };
+}
 function seedData() {
     console.info('Seeding data');
+    const dummyData = [];
+    for (let i = 1; i<=10; i++){
+      generateStory();
+    }
+    return Story.insertMany(dummyData);
 }
 
 function tearDownDb() {
@@ -34,5 +46,28 @@ describe('Hacker News API', function() {
 
   after(function() {
     return closeServer();
-  })
+  });
+
+  describe('POST endpoint', function() {
+    it('should post story', function () {
+      const newStory = generateStory();
+      return chai.request(app)
+      .post('/stories')
+      .send(newStory)
+      .then(function(res){
+        res.should.be.json;
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        res.body.should.include.keys('title', 'url', 'id', 'votes');
+        res.body.id.should.not.be.null;
+        res.body.title.should.equal(newStory.title);
+        res.body.url.should.equal(newStory.url);
+        return Story.findById(res.body.id);
+      })
+      .then(function(story){
+        story.title.should.equal(newStory.title);
+        story.url.shold.equal(newStory.url);
+      });
+    });
+  });
 });
